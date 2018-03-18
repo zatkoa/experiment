f625ddc9c37943458b2fdf160a276df5e761072a
zatkoa <z.andrej@azet.sk>
2018-03-18 23:39:21 +0100

    zachytavanie eventov

--- index.js
+++ index.js
@@ -1,23 +1,145 @@
 
-$(document).ready(function(){
-    $("p.nazov-produktu").each(function(){
-        a = $(this).text();
-        if (a.length > 50)
-            $(this).css("padding-top", "11px");
-    });
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
 
-    $(".next").click(function(){
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
+var item_id = 0;
+$(document).ready(function(){
+    $(".dalej").unbind().click(function(e){
         var url = new URL(window.location.href);
         var par =  parseInt(url.searchParams.get("q"));
+        var user_id =  parseInt(url.searchParams.get("userid"));
+
         if (isNaN(par))
-            par = 1;
-        else
-            par++;
-        window.location = "http://localhost/test/index.php?q=" + encodeURIComponent(par);
+            par = 0;
+
+        var values = {};
+
+        values.round = par;
+
+        values.user_id = user_id;
+        values.item_id = null;
+        values.event = 'skip';
+
+        values.category = $('.category').attr('id');
+        values.offset = $('.offset').attr('id');
+
+        var currentdate = new Date();
+        values.time = currentdate.getHours() + ":"
+            + currentdate.getMinutes() + ":"
+            + currentdate.getSeconds();
+
+        $.ajax({
+            url: "save.php",
+            type: "post",
+            async: true,
+            data: values,
+            success: function (response) {
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+            }
+        });
+
+        par++;
+
+        url = "http://localhost/test/index.php?q=" + encodeURIComponent(par);
+        url = url + "&userid=" + encodeURIComponent(user_id);
+        window.location = url;
     });
 
-    $('.col').on('click',function(){
+    $(".display").unbind().click(function(e){
+       $(".container").css("display","block");
+       $(".dalej").css("display","block");
+       $("a.display").css("display","none");
+
+        var values = {};
+        var url = new URL(window.location.href);
+        var user_id =  parseInt(url.searchParams.get("userid"));
+        var round =  parseInt(url.searchParams.get("q"));
+        if (isNaN(round))
+            round = 0;
+        values.round = round;
+        values.user_id = user_id;
+        values.item_id = null;
+        values.event = 'begin';
+
+        values.category = $('.category').attr('id');
+        values.offset = $('.offset').attr('id');
+
+        var currentdate = new Date();
+        values.time = currentdate.getHours() + ":"
+            + currentdate.getMinutes() + ":"
+            + currentdate.getSeconds();
+
+        $.ajax({
+            url: "save.php",
+            type: "post",
+            async: true,
+            data: values,
+            success: function (response) {
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+            }
+        });
+
+    });
+
+
+    $('.col').unbind().on('click',function(e) {
+
+        var values = {};
         var id = $(this).attr('id');
+        item_id = parseInt(id);
+        var url = new URL(window.location.href);
+        var round =  parseInt(url.searchParams.get("q"));
+        if (isNaN(round))
+            round = 0;
+        values.round = round;
+        values.user_id = parseInt(url.searchParams.get("userid"));
+        values.item_id = item_id;
+        values.event = 'detail';
+
+        var currentdate = new Date();
+        values.time = currentdate.getHours() + ":"
+            + currentdate.getMinutes() + ":"
+            + currentdate.getSeconds();
+
+        values.category = $('.category').attr('id');
+        values.offset = $('.offset').attr('id');
+
+        $.ajax({
+            url: "save.php",
+            type: "post",
+            async: true,
+            data: values,
+            success: function (response) {
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+            }
+        });
+
         var url = new URL(window.location.href);
         var par =  parseInt(url.searchParams.get("q"));
         var content = 'index.php';
@@ -27,8 +149,99 @@ $(document).ready(function(){
         } else
             content += '?id=' + encodeURIComponent(id);
 
-        $('.modal-content').load(content, function(){
+        $('#myModal .modal-content').load(content, function(){
             $('#myModal').modal();
+
+            $('#myModal').off('hidden.bs.modal').on('hidden.bs.modal', function (e) {
+                var url = new URL(window.location.href);
+                var round =  parseInt(url.searchParams.get("q"));
+                var currentdate = new Date();
+
+                if (isNaN(round))
+                    round = 0;
+
+                values.round = round;
+                values.user_id = parseInt(url.searchParams.get("userid"));
+                values.item_id = item_id;
+                values.time = currentdate.getHours() + ":"
+                    + currentdate.getMinutes() + ":"
+                    + currentdate.getSeconds();
+                values.event = 'close';
+
+                values.category = $('.category').attr('id');
+                values.offset = $('.offset').attr('id');
+
+                $.ajax({
+                    url: "save.php",
+                    type: "post",
+                    async:true,
+                    data: values ,
+                    success: function (response) {
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.log(textStatus, errorThrown);
+                    }
+                });
+
+            })
+
+            $("div.kupit").unbind().click(function(){
+                var values = {};
+                var currentdate = new Date();
+                var url = new URL(window.location.href);
+                var round =  parseInt(url.searchParams.get("q"));
+                if (isNaN(round))
+                    round = 0;
+
+                values.round = round;
+                values.user_id = parseInt(url.searchParams.get("userid"));
+                values.item_id = item_id;
+                values.time = currentdate.getHours() + ":"
+                    + currentdate.getMinutes() + ":"
+                    + currentdate.getSeconds();
+                values.event = 'nakup';
+
+                values.category = $('.category').attr('id');
+                values.offset = $('.offset').attr('id');
+
+                $.ajax({
+                    url: "save.php",
+                    type: "post",
+                    async:true,
+                    data: values ,
+                    success: function (response) {
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        console.log(textStatus, errorThrown);
+                    }
+                });
+
+                $(".container").css("display","none");
+                $(".dalej").css("display","none");
+                $('#myModal').modal('hide');
+                $('#dotaz').modal({backdrop: 'static', keyboard: false})
+
+                $('.odoslat').click(function(event) {
+                    var currentdate = new Date();
+
+                    var time = currentdate.getHours() + ":"
+                        + currentdate.getMinutes() + ":"
+                        + currentdate.getSeconds();
+                    var feedback = $('#feedback').val();
+                    var url = new URL(window.location.href);
+                    var par =  parseInt(url.searchParams.get("q"));
+                    var user_id = parseInt(url.searchParams.get("userid"));
+                    if (isNaN(par))
+                        par = 1;
+                    else
+                        par++;
+                    url = "http://localhost/test/index.php?q=" + encodeURIComponent(par);
+                    url = url + "&userid=" + encodeURIComponent(user_id);
+
+                    post(url, {feed: feedback, user_id: user_id, time: time, item_id: item_id});
+                });
+
+            });
         });
     });
 
--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 
 <?php
-
 session_start();
 // Connecting, selecting database
 $dbconn = pg_connect("host=localhost  dbname=experiment user=postgres")
 or die('Could not connect: ' . pg_last_error());
+
+if (isset($_POST['feed']) && isset($_POST['user_id']) && isset($_POST['time']) && isset($_POST['item_id']))
+{
+    $category_id = $_SESSION['category_index'];
+    $offset = $_SESSION['offset'];
+    $query = "INSERT INTO feed (feedback,time,user_id, item_id, category_id, \"offset\") VALUES 
+    ('$_POST[feed]','$_POST[time]','$_POST[user_id]', '$_POST[item_id]', '$category_id', '$offset')";
+    $result = pg_query($query);
+}
 if (isset($_GET['q']))
     $param =  $_GET['q'];
 else
     $param =  0;
 
+if ($param > 8) {
+    ?> <p> Experiment skončil. Ďakujem za tvoj čas!</p>
+    <?php
+    exit();
+}
 if ($param == 0) {
     $categories = array(139 => array(0, 10, 20), 1 => array(0, 10, 20), 36 => array(0, 10, 20));
     $categories_numbers = array(139, 1, 36);
@@ -21,20 +34,28 @@ else {
 
 if (isset($_GET['id'])) :
 
-    $all = $_SESSION['all'];
-    $icon_class = json_decode($all[$_GET['id']]['tags'],TRUE);
+    $query = 'SELECT * FROM products WHERE id =' . $_GET['id'];
+    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+    $line = pg_fetch_array($result, null, PGSQL_ASSOC);
+
+    $icon_class = json_decode($line['tags'],TRUE);
     ?>
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title"><?php echo $all[$_GET['id']]['title_deal'] ?></h5>
+            <h5 class="modal-title"><?php echo $line['title_deal'] ?></h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
-        <div class="img-detail"  style=" background-image:url('https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $all[$_GET['id']]['image_hd'] ?>');">
+        <div class="img-detail"  style=" background-image:url('https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $line['image_hd'] ?>');">
+        </div>
+        <div class="cena-kupit">
+            <div class="cena-modal">5.49 €</div>
+            <div class="kupit">Kúpiť</div>
         </div>
         <div class="modal-body">
-            <p><?php echo $all[$_GET['id']]['title_desc'] ?></p>
+            <p><?php echo $line['title_desc'] ?></p>
             <ul>
                 <?php
                 foreach ($icon_class as $key => $icon) {
@@ -49,7 +70,6 @@ if (isset($_GET['id'])) :
 <?php else :
 
 
-//print_r($categories);
 
 $categories_count =  count($categories);
 $random = random_int(1, $categories_count) - 1;
@@ -69,11 +89,8 @@ if (count($categories[$categories_index]) == 0) {
 
 $_SESSION['cat'] = $categories;
 $_SESSION['cat_num'] = $categories_numbers;
-//
-   /* echo $categories_index;
-    echo " ";
-    echo $index;
-    echo "/n";*/
+$_SESSION['category_index'] = $categories_index;
+$_SESSION['offset'] = $index;
 
 
 
@@ -82,24 +99,18 @@ $query = 'SELECT * FROM products WHERE category_id='. $categories_index .'  OFFS
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
 // Printing results in HTML
-//echo "<table>\n";
 $captions = array();
 $subscriptions = array();
 $images = array();
 $all = array();
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    //echo "\t<tr>\n";
-    //foreach ($line as $col_value) {
-        //echo "\t\t<td>" . $line['caption'] . "</td>\n";
-    //}
-   // echo "\t</tr>\n";
+
     array_push($captions, $line['title_short']);
     array_push($subscriptions, $line['title_short']);
-    array_push($images, $line['image']);
+    array_push($images, $line['image_hd']);
     array_push($all, $line);
 }
-$_SESSION['all'] = $all;
-//echo "</table>\n";
+
 // Free resultset
 //pg_free_result($result);
 
@@ -120,21 +131,28 @@ $_SESSION['all'] = $all;
     <title>Hello, world!</title>
 </head>
 <body style="height:100%">
-<svg class="next" height="100" width="100" style="position:fixed; top:50%; left:50%; transform: translate(-50%, -50%);">
-    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red"></circle>
-</svg>
+<a href="#" class="display"><svg height="100" width="100" style="position:fixed; top:50%; left:50%; transform: translate(-50%, -50%);">
+    <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red">
+    </circle>
+</svg></a>
+
+<a href="#" class="display"><svg height="100" width="100" style="position:fixed; top:50%; left:50%; transform: translate(-50%, -50%);">
+        <circle cx="50" cy="50" r="5" stroke="black" stroke-width="3" fill="red">
+        </circle>
+    </svg></a>
+<a href="#" class="dalej" ><span>Preskočiť</span></a>
 
 <div class="modal fade bd-example-modal-lg" tabindex="-1" id="myModal" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title"></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <p>Modal body text goes here.</p>
+                <p>Title</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary">Save changes</button>
@@ -145,84 +163,105 @@ $_SESSION['all'] = $all;
 </div>
 
 
+<div class="modal fade bd-example-modal-lg" tabindex="-1" id="dotaz" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Ďakujeme za Váš výber!</h5>
+            </div>
+            <div class="modal-body">
+                <p style="padding-bottom:20px; font-size: 28px;">Čo Vás na tejto ponuke zaujalo najviac?</p>
+                <form>
+                    <div class="form-group">
+                        <textarea name="feed" class="form-control" id="feedback" rows="3"></textarea>
+                    </div>
+                    <button type="button" style="margin:15px;" class="btn btn-secondary odoslat">Odoslať odpoveď</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="category" id="<?php echo $categories_index; ?>"></div>
+<div class="offset" id="<?php echo $index; ?>"></div>
 <div class="container" style="height:100%;     padding-top: 2.5%;
     padding-bottom: 2.5%;">
     <div class="row" style="height:calc( 100% / 3);">
-        <div class="col" id="0">
+        <div class="col" id="<?php echo $all[0]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[0] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[0] ?>" width="100%" class="img">
             </div>
-            <div class="cena"><span class="cena-produktu">Cena: 50.49 eur</span></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[0]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
-        <div class="col" id="1">
+        <div class="col" id="<?php echo $all[1]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[1] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[1] ?>" width="100%" class="img">
         </div>
-            <div class="popis"><p class="popis-produktu">V ruke orosený pohár plný kvasnicového piva a pred vami množstvo uvoľňujúcich procedúr... </p></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[1]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
-        <div class="col" id="2">
+        <div class="col" id="<?php echo $all[2]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[2] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[2] ?>" width="100%" class="img">
             </div>
-            <div class="cena"><span class="cena-produktu">Cena: 50.49 eur</span></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[2]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
-        <div class="col" id="3">
+        <div class="col" id="<?php echo $all[3]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[3] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[3] ?>" width="100%" class="img">
             </div>
-            <div class="cena"><span class="cena-produktu">Cena: 50.49 eur</span></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[3]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
     </div>
     <div class="row" style="height:calc( 100% / 3)";>
-        <div class="col" id="4">
+        <div class="col" id="<?php echo $all[4]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[4] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[4] ?>" width="100%" class="img">
             </div>
-            <div class="cena"><span class="cena-produktu">Cena: 50.49 eur</span></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[4]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
         <div class="col hide"></div>
         <div class="col hide"></div>
-        <div class="col" id="5">
+        <div class="col" id="<?php echo $all[5]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[5] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[5] ?>" width="100%" class="img">
             </div>
-            <div class="cena"><span class="cena-produktu">Cena: 50.49 eur</span></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[5]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
     </div>
     <div class="row" style="height:calc( 100% / 3)";>
-        <div class="col" id="6">
+        <div class="col" id="<?php echo $all[6]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[6] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[6] ?>" width="100%" class="img">
             </div>
-            <div class="cena"><span class="cena-produktu">Cena: 50.49 eur</span></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[6]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
-        <div class="col" id="7">
+        <div class="col" id="<?php echo $all[7]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[7] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[7] ?>" width="100%" class="img">
             </div>
-            <div class="cena"><span class="cena-produktu">Cena: 50.49 eur</span></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[7]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
-        <div class="col" id=8>
+        <div class="col" id="<?php echo $all[8]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[8] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[8] ?>" width="100%" class="img">
             </div>
-            <div class="cena"><span class="cena-produktu">Cena: 50.49 eur</span></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[8]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
-        <div class="col" id="9">
+        <div class="col" id="<?php echo $all[9]['id']; ?>">
             <div class="nazov"><p class="nazov-produktu"><?php echo $captions[9] ?></p></div>
             <div class="img">
                 <span class="helper"></span><img src="https://creati.cdn.platon.sk/zlavadna.sk/<?php echo $images[9] ?>" width="100%" class="img">
             </div>
-            <div class="cena"><span class="cena-produktu">Cena: 50.49 eur</span></div>
+            <div class="popis"><p class="popis-produktu"><?php echo mb_substr($all[9]['title_desc'],0,80, "utf-8") . "..."  ?></p></div>
         </div>
     </div>
 </div>
